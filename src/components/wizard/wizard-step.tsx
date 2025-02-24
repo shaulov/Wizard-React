87d05cc7414d4f4d762520da2731969062224833
shaulov <shineginn@gmail.com>
2025-02-24 21:45:53 +0300

    chore: install vite + react template

--- src/components/wizard/wizard-step.tsx
+++
@@ -1,9 +0,0 @@
-type WizardStepProps = {
-  children: JSX.Element;
-}
-
-function WizardStep ({ children }: WizardStepProps) {
-  return children;
-}
-
-export default WizardStep;
--- src/const.ts
+++
@@ -1,1 +0,0 @@
-export const STEP_LENGTH = 1;
