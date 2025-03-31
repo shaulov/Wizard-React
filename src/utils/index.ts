a4ee65e8e18b121c5458eb998eb47020545e671d
shaulov <shineginn@gmail.com>
2025-03-31 20:12:56 +0300

    chore: update compiler options

--- src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-import { config } from '../const.ts';
-
-export const getRequiredMessage = (name: string) => {
-    const item = config.find(item => item.id === name);
-
-    return item?.isRequired && item?.errorMessage;
-};
